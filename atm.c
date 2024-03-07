fe44bbea7a367a52b023d78401ee0dcb1bcb5f01
CodeWithArpit <150226804+arpitsharm@users.noreply.github.com>
2024-03-07 19:48:03 +0530

    Update atm.c

--- atm.c
+++ atm.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int pin, tf, choice;
+    int pin, tf, choice, lv=0;
     int amount = 100;
     for (int i = 0; i < 2; i++)
     {
@@ -24,7 +24,13 @@ int main()
     while (tf == 1)
     {
         printf("\nATM\n\n");
-        printf("1.Check Balance\n2.Withdrawn\n3.Deposit\n4.Exit\nEnter Your Choice :- \n");
+        if (lv == 1)
+        {
+            printf("Loan is InComplete\n");
+        }
+        
+        printf("Loan is InComplete\n");
+        printf("1.Check Balance\n2.Withdrawn\n3.Deposit\n4.Transfer\n5.Loan\n6.Exit\nEnter Your Choice :- \n");
         scanf("%d", &choice);
 
         if (choice == 1){
@@ -66,6 +72,49 @@ int main()
             }
 
             else if(choice == 4){
+                int amount_less2;
+
+                printf("Enter amount to Transfer :- ");
+                scanf("%d", &amount_less2);
+
+                if (amount_less2 < amount){
+                    int amount_greater;
+                    amount_greater = amount - amount_less2;
+                    amount = amount_greater;
+
+                    printf("Total Transfer Amount :- %d\n", amount_less2);
+                    printf("Total amount : %d\n", amount);
+                }
+
+                else{
+                    printf("Wrong Command\n");
+                }
+            }
+
+            else if(choice == 5){
+                int choice2, amounts;
+                printf("1.Loan Take\n2.Loan Dena\nEnter Choice :- ");
+                scanf("%d", &choice2);
+                if (choice2 == 1){
+                    printf("Enter Amount :- ");
+                    scanf("%d", &amounts);
+                    int amount_loan;
+                    amount_loan = amount + amounts;
+                    amount = amount_loan;
+                    lv = 1;
+                }
+
+                if (choice2 == 2){
+                    printf("Enter Amount :- ");
+                    scanf("%d", &amounts);
+                    int amount_loan;
+                    amount_loan = amount - amounts;
+                    amount = amount_loan;
+                    lv = 0;
+                }
+            }
+
+            else if(choice == 6){
                 tf = 0;
                 break;
             }
